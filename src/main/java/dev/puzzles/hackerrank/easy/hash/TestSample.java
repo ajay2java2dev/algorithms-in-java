a28395c07a1b8623c5d1e7a5f4291b8198581884
kamenon2 <kamenon2@illinois.edu>
2019-05-27 11:06:25 -0500

    Sherlocks failed algorithm

---
+++ src/main/java/dev/puzzles/hackerrank/easy/hash/TestSample.java
@@ -0,0 +1,26 @@
+package dev.puzzles.hackerrank.easy.hash;
+
+public class TestSample {
+
+  public static void main(String[] args) {
+    String ab = "ab";
+    String bc = "ba";
+
+    System.out.println("TestSample.main --> ab - bc = " + (ab.hashCode() - bc.hashCode()));
+    System.out.println("TestSample.main --> ab - bc = " + (bc.hashCode() - ab.hashCode()));
+
+    if (Math.abs(ab.hashCode()) == Math.abs(bc.hashCode())) {
+      System.out.println("ab is equals bc");
+    }
+
+    String bca = "bca";
+    String acb = "acb";
+
+    System.out.println("TestSample.main --> bca - acb = " + (bca.hashCode() - acb.hashCode()));
+    System.out.println("TestSample.main --> acb - bca = " + (acb.hashCode() - bca.hashCode()));
+
+    if (Math.abs(bca.hashCode()) == Math.abs(acb.hashCode())) {
+      System.out.println("ab is equals bc");
+    }
+  }
+}

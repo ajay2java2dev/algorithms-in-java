5454d13772702adb49a7cb67787040775783baff
Kalathil Ajay Menon <ajay.java.dev@gmail.com>
2019-06-23 19:00:47 -0500

    Stock Price not so simple

--- src/main/java/dev/puzzles/eopi/easy/StockPrice.java
+++ src/main/java/dev/puzzles/eopi/easy/StockPrice.java
@@ -1,5 +1,13 @@
 package dev.puzzles.eopi.easy;
 
+/*
+- The stock price problem is a basic problem mentioned in elements of programing interviews in
+ java.
+- The best stock trade needs to be filtered out where the array given is of the stock on daily
+basis.
+- The stock can only be sold or bought at the start of the day. Problem is to showcase the max
+benefit one could receive by buying and then selling the stock in any of the subsequent days.
+ */
 public class StockPrice {
 
 	public static float getBestPrice(float[] dailyStockPrices) {

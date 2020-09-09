1cf9f3986b6e5aadbb603beb314a9f15c9c46fe0
kamenon2 <kamenon2@illinois.edu>
2020-09-08 23:44:20 -0500

    Lets begin

---
+++ src/main/java/dev/algorithms/linkedlist/_1_SimpleLinkedList.java
@@ -0,0 +1,8 @@
+package dev.algorithms.linkedlist;
+
+public class _1_SimpleLinkedList {
+
+
+
+
+}
---
+++ src/main/java/dev/puzzles/here/and/there/medium/LongestSubstring.java
@@ -0,0 +1,16 @@
+package dev.puzzles.here.and.there.medium;
+
+//Given a string find the longest substring of unique characters
+public class LongestSubstring {
+
+    public int getLongestSubstring (String input) {
+        int longestLength = 0 ;
+
+        int [] index = new int [128];
+
+        for (int j = 0 ; )
+
+
+        return 0;
+    }
+}

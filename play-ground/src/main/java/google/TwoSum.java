e5eb8c0ba5a950641bcb2513c3a031d8c43cc03f
kamenon2 <kamenon2@illinois.edu>
2022-05-18 07:33:21 -0500

    opt-solution

--- play-ground/src/main/java/google/TwoSum.java
+++ play-ground/src/main/java/google/TwoSum.java
@@ -17,12 +17,42 @@ public class TwoSum {
         return indexMap;
     }
 
+    static Map<Integer, Integer> optimalSolution(int [] arr, int target) {
+        var indexMap = new HashMap<Integer, Integer>();
+        var keyValueMap = new HashMap<Integer, Integer>();
+        for (int i = 0 ; i < arr.length; i++) {
+            int numToFind = target - arr[i];
+            if (keyValueMap.containsKey(numToFind)) {
+                indexMap.put(keyValueMap.get(numToFind),i);
+            } else {
+                keyValueMap.put(arr[i], i);
+            }
+        }
+        return indexMap;
+    }
+
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5,6,7};
         int target = 10;
+
+        System.out.println("*****************************************************************");
+        System.out.println("Starting bruteforce ...");
+        var startTime = System.nanoTime();
         bruteForceSolution(arr, target).entrySet().forEach(integerIntegerEntry -> {
             System.out.format("\nstart_index: %d, end_index: %d\n",integerIntegerEntry.getKey(), integerIntegerEntry.getValue());
         });
+        var bruteTimeTaken = (System.nanoTime() - startTime);
+        System.out.println("Time taken (ns): " + (bruteTimeTaken));
 
+        System.out.println("*****************************************************************");
+        System.out.println("Starting optimal ...");
+        startTime = System.nanoTime();
+        optimalSolution(arr, target).entrySet().forEach(integerIntegerEntry -> {
+            System.out.format("\nstart_index: %d, end_index: %d\n",integerIntegerEntry.getKey(), integerIntegerEntry.getValue());
+        });
+        var optTimeTaken = (System.nanoTime() - startTime);
+        System.out.println("Time taken (ns): " + optTimeTaken);
+        System.out.println("*****************************************************************");
+        System.out.println("Total performace improvement with hashmap (in %): " + Float.valueOf(((float) bruteTimeTaken - (float)optTimeTaken)/(float)bruteTimeTaken) * 100);
     }
 }

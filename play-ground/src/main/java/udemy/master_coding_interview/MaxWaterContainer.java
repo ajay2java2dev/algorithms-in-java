90c86fc226c465e16d6f7d5ff3f860aff4ec43f9
Ajay <kalathilajay.menon@mrcooper.com>
2022-05-31 18:52:27 -0500

    code max water efficient

--- play-ground/src/main/java/udemy/master_coding_interview/MaxWaterContainer.java
+++ play-ground/src/main/java/udemy/master_coding_interview/MaxWaterContainer.java
@@ -40,12 +40,36 @@ public class MaxWaterContainer {
                 }
             }
         }
-
         return maxPair;
+    }
+
+    static Pair<Integer, Integer, Integer> getMaxWaterContainerEfficient (int [] heights) {
+        int p1 = 0, p2 = heights.length - 1, maxArea = 0;
+        Pair<Integer, Integer, Integer> pair = null;
+        while (p1 < p2) {
+            int minHeight = Math.min(heights[p1], heights[p2]);
+            int distance = p2 - p1;
+            int area = minHeight * distance;
+
+            if (maxArea < area) {
+                maxArea = area;
+                pair = new Pair<Integer, Integer, Integer>(p1, p2, maxArea);
+            }
+
+            if (heights[p1] < heights[p2]) {
+                p1++;
+            } else {
+                p2--;
+            }
+        }
+        return pair;
     }
 
     public static void main(String[] args) {
         int [] heights = {1,8,6,2,9,4};
-        System.out.println("Max water pair: " + getMaxWaterContainerBrute(heights));
+        long startTime = System.nanoTime();
+        System.out.println("Max water pair: " + getMaxWaterContainerBrute(heights) + " , time_taken: " + (System.nanoTime() - startTime));
+        startTime = System.nanoTime();
+        System.out.println("Max water pair: " + getMaxWaterContainerEfficient(heights) + " , time taken: " + (System.nanoTime() - startTime));
     }
 }
--- play-ground/src/test/java/features/jvm9/ReactiveStreamsDemo.java
+++ play-ground/src/test/java/features/jvm9/ReactiveStreamsDemo.java
@@ -1,2 +1,42 @@
-package features.jvm9;public class ReactiveStreamsDemo {
+package features.jvm9;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.function.Supplier;
+
+public class ReactiveStreamsDemo {
+
+    public static void main(String[] args) {
+
+        Supplier<? extends String> randomWeatherSupplier = () -> List.of("Sunny", "Rainy", "Cloudy", "Partial-Cloudy", "Thunderstorm", "Tornado")
+                .get(new Random().nextInt(1) + (new Random().nextInt(90))) + "F";
+
+        //TODO::  Complete implentation
+
+    }
+
+    public static class PeriodicPublisher<T> extends SubmissionPublisher<T> {
+        final ScheduledFuture<?> periodicTask;
+        final ScheduledExecutorService scheduler;
+
+        public PeriodicPublisher(Executor executor, int maxBufferCapacity, Supplier<? extends T> supplier,
+                                 long period, TimeUnit unit) {
+            super(executor, maxBufferCapacity);
+            this.scheduler = new ScheduledThreadPoolExecutor(1);
+            this.periodicTask = scheduler.scheduleAtFixedRate(() -> submit(supplier.get()), 0, period, unit);
+        }
+
+        @Override
+        public void subscribe(Flow.Subscriber<? super T> subscriber) {
+            super.subscribe(subscriber);
+        }
+
+        @Override
+        public void close() {
+            periodicTask.cancel(false);
+            scheduler.shutdown();
+            super.close();
+        }
+    }
 }

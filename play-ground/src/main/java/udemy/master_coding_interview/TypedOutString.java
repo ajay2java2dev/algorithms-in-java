ac809a2451761603cdfa0ef57f137125b8fe9256
Ajay <kalathilajay.menon@mrcooper.com>
2022-06-02 13:36:23 -0500

    Another Typed out string using stack - O (2n) but still not good

--- play-ground/src/main/java/udemy/master_coding_interview/TypedOutString.java
+++ play-ground/src/main/java/udemy/master_coding_interview/TypedOutString.java
@@ -6,6 +6,9 @@ import java.util.*;
 
 public class TypedOutString {
 
+
+    //this doesnt seems good enough: https://leetcode.com/submissions/detail/712940806/
+    //but used list iterator for the first time.
     static boolean compareTypedOutStrings (String s1, String s2) {
 
         List<String> s1List = Arrays.asList(s1.split(""));
@@ -24,7 +27,6 @@ public class TypedOutString {
                     s1Iterator.remove();
                 }
             }
-
         }
 
         ListIterator<String> s2Iterator = s2LinkedList.listIterator();
@@ -43,6 +45,29 @@ public class TypedOutString {
         return s1New.equals(s2New);
     }
 
+    static String buildTypedOutString (String inputString) {
+        Stack<String> stringStack = new Stack<>();
+        String [] inputArray = inputString.split("");
+        for (int i = 0; i < inputArray.length; i++) {
+            if ("#".equalsIgnoreCase(inputArray[i])) {
+                if (!stringStack.isEmpty()) //purposefully did this inside.
+                    stringStack.pop();
+            } else {
+                stringStack.push(inputArray[i]);
+            }
+        }
+        return stringStack.toString();
+    }
+
+    //another brute approach using stack
+    static boolean compareTypedOutStringByStack (String s1, String s2) {
+        String s1Out = buildTypedOutString(s1);
+        String s2Out = buildTypedOutString(s2);
+        return s1Out.equals(s2Out);
+    }
+
+
+
     public static void main(String[] args) {
         String s1 = "ab#z";
         String s2 = "az#z";
@@ -51,5 +76,9 @@ public class TypedOutString {
         String s3 = "ab##z";
         String s4 = "az#z";
         Assertions.assertFalse(compareTypedOutStrings(s3, s4));
+
+        String s5 = "y#fo##f";
+        String s6 = "y#f#o##f";
+        Assertions.assertTrue(compareTypedOutStringByStack(s5, s6));
     }
 }
